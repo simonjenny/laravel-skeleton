443f035dc4b59dad0704f4234c352a4074e468ed
Simon Jenny <simon.jenny@kuvvu.ch>
2026-01-17 00:39:19 +0100

    Update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../app/Routes/web.php',
-        commands: __DIR__.'/../app/Routes/console.php'
+        commands: __DIR__.'/../app/Routes/console.php',
+        health: '/up', 
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
